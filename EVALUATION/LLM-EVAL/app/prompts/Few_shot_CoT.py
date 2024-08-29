86df5c24c0a4a5a4c93bd5b596e29c37a4232f26
Vinod <vinod.v.p@gmail.com>
2024-08-30 01:17:46 +0200

    Few Shot CoT

--- EVALUATION/LLM-EVAL/app/prompts/Few_shot_CoT.py
+++ EVALUATION/LLM-EVAL/app/prompts/Few_shot_CoT.py
@@ -13,6 +13,9 @@ Entity types can strictly be "person" for any kind of user, "component" for a so
 , or "system" for an entity that contains multiple components, and is typically a framework. You may ignore other types of entities.
 Entity has to be uniquely named.
 
+For each relationship that you detect, ensure that the corresponding entities are also included in the entities section. It is possible that an entity may be referred 
+to later on using an abbreviation or shorter version of its name. Try to match any given entity name with that of entities that you already detected.
+
 For each entity that you identified, you try to identify whether it contains other entities, or whether this entity is a part of another entity.
 Each entity may also be calling another entity. 
 Each such identified relationship will a set of the form [ENTITY_1, RELATIONSHIP, ENTITY_2, DESCRIPTION] between two named entities.
@@ -23,19 +26,58 @@ Relationship can only be one of "contains","part-of", "calls" or "called-from".
     "calls": if entity_1 initiates a connection to entity_2.
     "called-from" : if entity_1 is called or invoked from entity_2.
 You should include each pair of entities for which you can find such a relationship. The order of entity_1 and entity_2 is important, to ensure the direction implied 
-in the relationship type is exactly as in the text. 
+in the relationship type is exactly as in the text.
 The relationships "part-of" and "contains" are inverse of each other, so for 2 components, X and Y, X "part-of" Y and Y "contains" X are the same. 
-So, you can skip one of these redundant relatonships in the JSON.
+So, you can skip one of these redundant relationships in the JSON.
 
     
 Don't make anything up and don't add any extra data. If you can't find any data for a entity or a relationship don't include it.
 Only add entities and relationships that are part of the text.
 
-Let me give you a few examples:
-Example 1: ""
-Output for Example 1: ""
-Example 2: ""
-Output for Example 2: ""
+Let me give you a few examples each of which includes the user input, the corresponding output JSON, and the step-by-step reasoning for the output:
+
+Example 1: 
+User Input for example 1: "A time-based Amazon EventBridge scheduler invokes an AWS Lambda function to populate search indexes with multimodal embeddings and product meta-data.
+The Lambda function first retrieves product feed stored as a JSON file in Amazon Simple Storage Service (Amazon S3)."
+Output JSON for example 1: "{{
+  "entities": [
+    ["Amazon EventBridge scheduler", "system", "invokes an AWS Lambda function to populate search indexes with multimodal embeddings and product meta-data"],
+    ["AWS Lambda function", "system", "retrieves product feed stored as a JSON file in Amazon Simple Storage Service"],
+    ["Amazon Simple Storage Service", "system", "JSON file storage containing product feed"],
+  ],
+  "relationships": [
+    ["Amazon EventBridge scheduler", "calls", "AWS Lambda function", "to populate search indexes with multimodal embeddings and product meta-data."],
+    ["Amazon Simple Storage Service", "called-from","AWS Lambda function", "retrieves product feed stored as a JSON file in Amazon Simple Storage Service (Amazon S3)" ],
+  ]
+}}"
+Reasoning for example 1: 
+From the sentence "A time-based Amazon EventBridge scheduler invokes an AWS Lambda function to populate search indexes with multimodal embeddings and product meta-data.", two named entities "Amazon EventBridge scheduler" and "AWS Lambda function" can be detected. The word invokes implies that the former entity has a relationship type "calls" with the latter entity.
+From the sentence "The Lambda function first retrieves product feed stored as a JSON file in Amazon Simple Storage Service (Amazon S3).", 2 entities are mentioned. The entity "Lambda function" here can be interpreted as the same as that of the previously detected "AWS Lambda function" entity, and there can be reused, without a new entity. Also, a new entity "Amazon Simple Storage Service" that was abbreviated(using brackets) as "Amazon S3", is mentioned. This means that  Amazon Simple Storage Service contains JSON files, these files are used to store product feed data, and the Lambda function retrieves the product feed data. So, The relationship between the 2 entities "AWS Lambda function" and "Amazon Simple Storage Service" can be intepreted that the former "calls" the latter, or otherwise the latter is "called-from" the former.  
+
+
+Example 2: 
+User Input for example 2: "The data in Amazon Simple Storage Service (Amazon S3) is linked to an Amazon Athena database, which runs queries against this data and returns query results to Amazon QuickSight.
+
+QuickSight obtains the query results and builds dashboard visualizations for your management team."
+Output JSON for example 2: "{{
+  "entities": [
+    ["Amazon S3", "system", "uses an AWS Lambda function for data transformation"],
+    ["Amazon Athena database", "system", "runs queries against this data and returns query results"],
+    ["Amazon QuickSight", "system", "obtains the query results and builds dashboard visualizations"],
+    ["Management Team", "person", "uses dashboard visualizations"],
+  ],
+  "relationships": [
+    ["Amazon S3", "called-from", "Amazon Athena database", "Amazon Athena database, which runs queries against the data in in Amazon Simple Storage Service"],
+    ["Amazon Athena database", "calls", "Amazon QuickSight", "returns query results"],
+    ["Amazon QuickSight", "called-from", "Management Team", "contains dashboard visualizations built for management Team"]
+  ]
+}}"
+Reasoning for example 2:
+
+The sentence "The data in Amazon Simple Storage Service (Amazon S3) is linked to an Amazon Athena database, which runs queries against this data and returns query results to Amazon QuickSight." contains references to 3 entities "Amazon S3" ( using "Amazon Simple Storage Service"  as the entity name instead of "Amazon S3" would have been also correct), "Amazon Athena database" and "Amazon QuickSight". It can be interpreted that Amazon Athena database runs queries against the data in Amazon S3, the relationship between these 2 entities is that "Amazon S3" is "called-from" "Amazon Athena database". Also, from "Amazon Athena database, which runs queries against this data and returns query results to Amazon QuickSight.", it can be deduced that "Amazon Athena database" "calls" "Amazon QuickSight" to return the query results.
+
+In the sentence "QuickSight obtains the query results and builds dashboard visualizations for your management team." , the entity Quicksight can inferred as referring to "Amazon QuickSight", and that it provides dashboards which are consumed by the management team. Managament Team is a user group, and therefore a "person" type, and therefore the relationship between them can be interpreted as Quickstight is "called-from" the Management team.
+ 
 </s>
 
 Question: Generate the JSON response based on the following text: 
